4313afb1cb04de370850f7de86caddf0d741f449
prasun-biswas <rpblikescodes@gmail.com>
2015-03-17 16:53:38 +0600

    xcfxvv
    xvxv

---
+++ CircleApp/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Circle
+    {
+        public double Radius { set; get; }
+        public double Area { set; get; }
+        public double Perimeter { set; get; }
+        public double diameter { set; get; }
+    }
+}
